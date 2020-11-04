508da5b67e72fa8d7074c9336a3e0ca46c1440b3
Yan Shatskiy <writemetolinkedin@git.io>
2020-11-04 16:56:31 +0100

    javascript tasks - sort-string pick omit

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,27 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param) {
+  if (param == "desc") {
+    return arr.slice().sort((prev, next) => {
+      if (prev.toLowerCase() === next.toLowerCase()) {
+        if (prev[0] > next[0]) {
+          return -1;
+        }
+      } else {
+        return next.localeCompare(prev);
+      }
+    });
+  } else {
+    return arr.slice().sort((prev, next) => {
+      if (prev.toLowerCase() === next.toLowerCase()) {
+        if (prev[0] < next[0]) {
+          return -1;
+        }
+      } else {
+        return prev.localeCompare(next);
+      }
+    });
+  }
 }
+
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,22 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const arr = Object.entries(obj);
+  const newObj = {};
+  let index = 0;
+  for (let value of arr) {
+    if (fields.includes(value[0])) {
+      newObj[value[0]] = arr[index][1];
+    }
+    index++;
+  }
+  return newObj;
 };
+
+const fruits = {
+  apple: 2,
+  orange: 4,
+  banana: 3,
+};
+
+console.log(pick(fruits, "apple", "banana", "dff", "erty")); // { apple: 2, banana: 3 }
