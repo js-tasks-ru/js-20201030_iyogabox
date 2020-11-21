308a8087e65f73488c9ba21bb1a067e581a4bc26
Yan Shatskiy <writemetolinkedin@git.io>
2020-11-21 19:59:21 +0100

    The task 2 - The Tooltip was realeased

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,63 @@
 class Tooltip {
+	static instance;
+	element;
 
-}
+	showTooltip = (event) => {
+	  const currentDataset = event.target.closest("[data-tooltip]");
+	  if (currentDataset) {
+		this.render(currentDataset.dataset.tooltip);
+		document.addEventListener("pointermove", this.onMouseMove);
+	  }
+	};
+	onMouseMove = (event) => {
+	  this.moveTooltip(event);
+	};
+	moveTooltip(event) {
+	  const left = event.clientX + 10;
+	  const top = event.clientY + 10;
+	  this.element.style.left = `${left}px`;
+	  this.element.style.top = `${top}px`;
+	}
 
-const tooltip = new Tooltip();
+	removeTooltip = () => {
+	  if (this.element) {
+		this.element.remove();
+	  }
+	};
+	removeLisener() {
+	  document.removeEventListener("pointerover", this.showTooltip);
+	  console.log("Voice");
+	}
+	constructor() {
+	  if (Tooltip.instance) {
+		return Tooltip.instance;
+	  }
+	  Tooltip.instance = this;
+	}
 
-export default tooltip;
+	eventListener() {
+	  document.addEventListener("pointerover", this.showTooltip);
+	  document.addEventListener("pointerout", this.removeTooltip);
+	}
+
+	initialize() {
+	  this.eventListener();
+	}
+	render(html) {
+	  this.element = document.createElement("div");
+	  this.element.className = "tooltip";
+	  this.element.innerHTML = html;
+	  document.body.append(this.element);
+	}
+
+	destroy() {
+	  document.removeEventListener("pointerover", this.showTooltip);
+	  document.removeEventListener("pointerout", this.removeTooltip);
+	  this.removeTooltip();
+	}
+  }
+
+  const tooltip = new Tooltip();
+
+  export default tooltip;
+
